46bf97ebda69c071b8ed3efb89a08fa35ba7dc6d
Camilo Albarran <camilofam@gmail.com>
2018-05-13 18:08:07 -0400

    Editar modificar articulos

--- public/js/app.js
+++ public/js/app.js
@@ -34368,20 +34368,24 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
                 console.log(error);
             });
         },
-        actualizarCategoria: function actualizarCategoria() {
-            if (this.validarCategoria()) {
+        actualizarArticulo: function actualizarArticulo() {
+            if (this.validarArticulo()) {
                 return;
             }
 
             var me = this;
 
-            axios.put('/categoria/actualizar', {
+            axios.put('articulo/actualizar', {
+                'idcategoria': this.idcategoria,
+                'codigo': this.codigo,
                 'nombre': this.nombre,
+                'stock': this.stock,
+                'precio_venta': this.precio_venta,
                 'descripcion': this.descripcion,
-                'id': this.categoria_id
+                'id': this.articulo_id
             }).then(function (response) {
                 me.cerrarModal();
-                me.listarCategoria(1, '', 'nombre');
+                me.listarArticulo(1, '', 'nombre');
             }).catch(function (error) {
                 console.log(error);
             });
